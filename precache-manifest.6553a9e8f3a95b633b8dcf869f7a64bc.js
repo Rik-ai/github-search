6e0fe06d0508217e682ae447e445e8fc251c5949
Ivan <ivan.lakhanin@gmail.com>
2020-08-29 21:48:15 +0300

    Updates

---
+++ precache-manifest.6553a9e8f3a95b633b8dcf869f7a64bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d264564793733929052345472a5a999",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "cac1a01a67eeab65ed4d",
+    "url": "/github-search/static/css/main.98ab1443.chunk.css"
+  },
+  {
+    "revision": "e0aa9fea77b18099c776",
+    "url": "/github-search/static/js/2.5245234f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-search/static/js/2.5245234f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac1a01a67eeab65ed4d",
+    "url": "/github-search/static/js/main.c440115f.chunk.js"
+  },
+  {
+    "revision": "ff3e0a04c97ea10dd625",
+    "url": "/github-search/static/js/runtime-main.47b540af.js"
+  }
+]);
